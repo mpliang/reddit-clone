4fca7ebdcc5c667807f030752720be82f667f455
cnichols <mellowdious@mail.com>
2015-10-01 17:21:56 -0700

    added swal

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('reddit')
-.controller('UsersCtrl', function($scope, $state, Auth){
+.controller('UsersCtrl', function($scope, $state, $window, Auth){
   $scope.state = $state.current.name.split('.')[1].toUpperCase();
 
   $scope.submit = function(user) {
@@ -9,18 +9,23 @@ angular.module('reddit')
       if(user.password1 !== user.password2){
         $scope.user = {};
         $scope.user.email = user.email;
-        alert("Passwords do not match.")
+        $window.swal("Passwords do not match.")
       } else {
         Auth.createUser(user)
         .then(function(userData){
           $state.go('users.login');
         }).catch(function(err) {
           $scope.user = {};
-          alert(err);
+          $window.swal({
+            title: "Oh no!",
+            text: err,
+            type: 'error',
+            animation: "slide-from-top"
+          });
         });
-
       }
     } else {
+      
       // login stuff
     }
   };
