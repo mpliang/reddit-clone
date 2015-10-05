743157e5c9f7a5d4839fe8f2d54431cccbbe7d06
Michael Liang <michaelpliang@gmail.com>
2015-10-05 10:57:11 -0700

    moved form back

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -4,38 +4,38 @@ angular.module('reddit')
 .controller('UsersCtrl', function($scope, $state){
   $scope.state = $state.current.name.split('.')[1].toUpperCase();
 
-  // $scope.submit = function(user) {
-  //   if($scope.state === 'REGISTER') {
-  //     if(user.password1 !== user.password2){
-  //       $scope.user = {};
-  //       $scope.user.email = user.email;
-  //       $window.swal({
-  //         title: "Oh no!",
-  //         text: "Passwords do not match.",
-  //         type: 'error'
-  //       });
-  //     } else {
-  //       Auth.createUser(user)
-  //       .then(function(userData){
-  //         $state.go('users.login');
-  //       }).catch(function(err) {
-  //         $scope.user = {};
-  //         $window.swal({
-  //           title: "Oh no!",
-  //           text: err,
-  //           type: 'error',
-  //           animation: "slide-from-top"
-  //         });
-  //       });
-  //     }
-  //   } else {
-  //     Auth.login(user)
-  //     .then(function(userData){
-  //       $state.go('index');
-  //     })
-  //     .catch(function(err) {
-  //       console.error(err);
-  //     });
-  //   }
-  // };
+  $scope.submit = function(user) {
+    if($scope.state === 'REGISTER') {
+      if(user.password1 !== user.password2){
+        $scope.user = {};
+        $scope.user.email = user.email;
+        $window.swal({
+          title: "Oh no!",
+          text: "Passwords do not match.",
+          type: 'error'
+        });
+      } else {
+        Auth.createUser(user)
+        .then(function(userData){
+          $state.go('users.login');
+        }).catch(function(err) {
+          $scope.user = {};
+          $window.swal({
+            title: "Oh no!",
+            text: err,
+            type: 'error',
+            animation: "slide-from-top"
+          });
+        });
+      }
+    } else {
+      Auth.login(user)
+      .then(function(userData){
+        $state.go('index');
+      })
+      .catch(function(err) {
+        console.error(err);
+      });
+    }
+  };
 });
